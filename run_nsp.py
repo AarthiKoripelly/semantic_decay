8b8732b629597357e93b0bf2d5240222dc713ce9
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-01-02 20:57:13 -0600

    basic run_nsp script coming along, now need to add in full year processing power

--- run_nsp.py
+++ run_nsp.py
@@ -1,60 +1,71 @@
 from transformers import BertConfig
-config = BertConfig.from_pretrained("bert-base-uncased")
-
+import random
 from transformers import BertTokenizer, BertForNextSentencePrediction
 import torch
+from tqdm import tqdm  # for our progress bar
 
+config = BertConfig.from_pretrained("bert-base-uncased")
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 #This will instantiatiate a pre-trained model for NSP
-#TODO (MS): load a given pretrained model
-model = BertForNextSentencePrediction.from_pretrained('/grand/projects/SuperBERT/mansisak/semantic_decay_models/bookcorpus_pretraining/')
+model_path = '/grand/projects/SuperBERT/mansisak/semantic_decay_models/bookcorpus_pretraining/'
+def get_model(model_path):
+    model = BertForNextSentencePrediction.from_pretrained('/grand/projects/SuperBERT/mansisak/semantic_decay_models/bookcorpus_pretraining/')
+    return model
 
-#TODO (MS): fix path to data
-file_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/2008/Biology/20220701_070950_00030_vrytk_d58b1dad-8368-42de-b191-b9a2da885938.txt"
-with open(file_path, "r") as fp:
-  abstracts = fp.read().split('\n\n')
-
-print("Opened Abstracts")
-
-bag = [item for abstract in abstracts for item in abstract.split('\n') if item != '']
-bag_size = len(bag)
+model = get_model(model_path)
 
 
-import random
+data_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/2008/Biology/20220701_070950_00030_vrytk_d58b1dad-8368-42de-b191-b9a2da885938.txt"
+def get_data(data_path):
+    with open(data_path, "r") as fp:
+      abstracts = fp.read().split('\n\n')
 
-sentence_a = []
-sentence_b = []
-label = []
-total_sentences = 0
-for abstract in abstracts:
-    sentences = [
-        sentence for sentence in abstract.split('\n') if sentence != ''
-    ]
-    num_sentences = len(sentences)
-    total_sentences += num_sentences
-    if num_sentences > 1:
-        #TODO: this is wasting data, make sure every sentence in each abstract is used
-        start = random.randint(0, num_sentences-2)
-        # 50/50 whether is IsNextSentence or NotNextSentence
-        if random.random() >= 0.5:
-            # this is IsNextSentence
-            sentence_a.append(sentences[start])
-            sentence_b.append(sentences[start+1])
-            label.append(0)
-        else:
-            index = random.randint(0, bag_size-1)
-            # this is NotNextSentence
-            #TODO: make sure this is actually non-sequential sentences!!
-            sentence_a.append(sentences[start])
-            sentence_b.append(bag[index])
-            label.append(1)
+    bag = [item for abstract in abstracts for item in abstract.split('\n') if item != '']
+    bag_size = len(bag)
+    return bag, bag_size, abstracts
 
-print("Finished Processing All Abstracts")
+bag, bag_size, abstracts = get_data(data_path)
 
-inputs = tokenizer(sentence_a, sentence_b, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
 
-inputs['labels'] = torch.LongTensor([label]).T
+def process_abstracts(abstracts):
+    sentence_a = []
+    sentence_b = []
+    label = []
+    total_sentences = 0
+    for abstract in abstracts:
+        sentences = [
+            sentence for sentence in abstract.split('\n') if sentence != ''
+        ]
+        num_sentences = len(sentences)
+        total_sentences += num_sentences
+        if num_sentences > 1:
+            #TODO: this is wasting data, make sure every sentence in each abstract is used
+            start = random.randint(0, num_sentences-2)
+            # 50/50 whether is IsNextSentence or NotNextSentence
+            if random.random() >= 0.5:
+                # this is IsNextSentence
+                sentence_a.append(sentences[start])
+                sentence_b.append(sentences[start+1])
+                label.append(0)
+            else:
+                index = random.randint(0, bag_size-1)
+                # this is NotNextSentence
+                #TODO: make sure this is actually non-sequential sentences!!
+                sentence_a.append(sentences[start])
+                sentence_b.append(bag[index])
+                label.append(1)
+    print("Finished Processing All Abstracts")
+    return sentence_a, sentence_b, label
+
+sentence_a, sentence_b, label = process_abstracts(abstracts)
+
+def get_inputs(sentence_a, sentence_b, label):
+    inputs = tokenizer(sentence_a, sentence_b, return_tensors='pt', max_length=512, truncation=True, padding='max_length')
+    inputs['labels'] = torch.LongTensor([label]).T
+    return inputs
+
+inputs = get_inputs(sentence_a, sentence_b, label)
 
 class AbstractsDataset(torch.utils.data.Dataset):
     def __init__(self, encodings):
@@ -71,65 +82,70 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # and move our model over to the selected device
 model.to(device)
 
-# activate training mode
-model.train()
-# initialize optimizer
-optim = torch.optim.AdamW(model.parameters(), lr=5e-6)
-
-from tqdm import tqdm  # for our progress bar
-
 epochs = 1
+def train_model(epochs, model, loader):
+    # activate training mode
+    model.train()
+    # initialize optimizer
+    optim = torch.optim.AdamW(model.parameters(), lr=5e-6)
 
-for epoch in range(epochs):
-    # setup loop with TQDM and dataloader
-    loop = tqdm(loader, leave=True)
-    for batch in loop:
-        # initialize calculated gradients (from prev step)
-        optim.zero_grad()
-        # pull all tensor batches required for training
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
-        # process
-        outputs = model(input_ids, attention_mask=attention_mask,
-                        labels=labels)
-        # extract loss
-        loss = outputs.loss
-        # calculate loss for every parameter that needs grad update
-        loss.backward()
-        # update parameters
-        optim.step()
-        # print relevant info to progress bar
-        loop.set_description(f'Epoch {epoch}')
-        loop.set_postfix(loss=loss.item())
+    for epoch in range(epochs):
+        # setup loop with TQDM and dataloader
+        loop = tqdm(loader, leave=True)
+        for batch in loop:
+            # initialize calculated gradients (from prev step)
+            optim.zero_grad()
+            # pull all tensor batches required for training
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            # process
+            outputs = model(input_ids, attention_mask=attention_mask,
+                            labels=labels)
+            # extract loss
+            loss = outputs.loss
+            # calculate loss for every parameter that needs grad update
+            loss.backward()
+            # update parameters
+            optim.step()
+            # print relevant info to progress bar
+            loop.set_description(f'Epoch {epoch}')
+            loop.set_postfix(loss=loss.item())
+    #TODO (MS): save checkpointed model
+    return model
 
-#TODO: need to evaluate model accuracy
-model.eval()
 
-total_pairs = 0
-correct_classifications = 0;
-with torch.no_grad():
-    for batch in tqdm(loader, leave=True):
-        # initialize calculated gradients (from prev step)
-        # pull all tensor batches required for training
-        input_ids = batch['input_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        labels = batch['labels'].to(device)
-        # process
-        outputs = model(input_ids, attention_mask=attention_mask,
-                        labels=labels)
-        # extract loss
-        loss = outputs.loss
+def eval_model(model, loader):
+    model.eval()
 
-        correct_classifications += labels.shape[0] - torch.sum(torch.abs(torch.argmax(outputs.logits, axis=1) - labels.squeeze())).item()
-        total_pairs += labels.shape[0]
+    total_pairs = 0
+    correct_classifications = 0;
+    with torch.no_grad():
+        loop = tqdm(loader, leave=True)
+        for batch in loop:
+            # initialize calculated gradients (from prev step)
+            # pull all tensor batches required for training
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+            # process
+            outputs = model(input_ids, attention_mask=attention_mask,
+                            labels=labels)
+            # extract loss
+            loss = outputs.loss
 
-        # print relevant info to progress bar
-        loop.set_description(f'Evaluation')
-        loop.set_postfix(accuracy=correct_classifications/total_pairs)
-        #loop.set_postfix(loss=loss.item())
+            correct_classifications += labels.shape[0] - torch.sum(torch.abs(torch.argmax(outputs.logits, axis=1) - labels.squeeze())).item()
+            total_pairs += labels.shape[0]
 
-accuracy = correct_classifications / total_pairs
-print("accuracy: ", accuracy)
+            # print relevant info to progress bar
+            loop.set_description(f'Evaluation')
+            loop.set_postfix(accuracy=correct_classifications/total_pairs)
+            #loop.set_postfix(loss=loss.item())
 
-#TODO: 
+    accuracy = correct_classifications / total_pairs
+    print("accuracy: ", accuracy)
+
+
+eval_model(model, loader)
+model = train_model(epochs, model, loader) 
+eval_model(model, loader)
