ecd9401490e782af5e62893322a844446137a161
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2023-01-04 13:41:13 -0600

    finished modularizing training/experimental code, need to add checkpointing

--- run_nsp.py
+++ run_nsp.py
@@ -8,70 +8,45 @@ from model.get_model import get_model
 from data.load_data import get_data, process_abstracts, get_inputs, AbstractsDataset
 from train.trainer import train_model, eval_model
 
+#TODO(MS): Need to seed everything to ensure reproducibility
+
 def main(args):
     pretrained_model_path = args.pretrained_model_path
     batch_size= args.batch_size
     epochs = args.epochs
     start_year = args.start_year
     end_year = args.end_year
+    base_data_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/"
+    subject = "Computer-Science"
 
     model = get_model(pretrained_model_path)
 
 
 
     for y in range(start_year, end_year+1):
-        base_data_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/"
+        #TODO(MS): load model from checkpoint if applicable
         year = str(y)
-        #subject = "Philosophy"
-        subject = "Computer-Science"
-        data_dir_path = base_data_path + year + "/" + subject + "/"
 
-        #TODO (MS): load checkpointed model by subject and year!!
-        for filename in os.listdir(data_dir_path):
-            data_path = os.path.join(data_dir_path, filename)
-            # checking if it is a file
-            if os.path.isfile(data_path):
-                print(data_path)
-
-                bag, bag_size, abstracts = get_data(data_path)
-                sentence_a, sentence_b, label = process_abstracts(abstracts, bag, bag_size)
-                if(len(sentence_a) <= 0):
-                    print("DATA LEN: ", len(sentence_a))
-                    continue
-                inputs = get_inputs(sentence_a, sentence_b, label)
-                dataset = AbstractsDataset(inputs)
-                loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-                model = train_model(epochs, model, loader) 
+        model = train_model(epochs, model, batch_size, base_data_path, year, subject)
 
         #Create Checkpoint by subject and year
         checkpoints_path = '/grand/projects/SuperBERT/mansisak/semantic_decay_models/checkpoints/'
         checkpoint_dir_path = checkpoints_path + year + "/" + subject + "/"
         model.save_pretrained(checkpoint_dir_path)
 
-    base_data_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/"
     year = str(1931)
-    subject = "Computer-Science"
-
     eval_model(model, batch_size, base_data_path, year, subject)
-    """
 
-    base_data_path = "/grand/projects/SuperBERT/mansisak/bert-abstracts/"
-    year = str(1932)
-    subject = "Biology"
-
-    eval_model(model, base_data_path, year, subject)
-
-    """
     return 0
 
 #Set Arguments
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Semantic Decay NSP Fine Tuning Arguments')
+    #TODO (MS): add arg for learning rate
     parser.add_argument('--batch_size', type=int, default=16, help='Batch Size')
     parser.add_argument('--epochs', type=int, default=1, help='Epochs')
     parser.add_argument('--start_year', type=int, default=1931, help='Year for which to begin training')
-    parser.add_argument('--end_year', type=int, default=1932, help='Year for which to end training')
+    parser.add_argument('--end_year', type=int, default=1933, help='Year for which to end training')
     parser.add_argument('--pretrained_model_path', type=str, default='/grand/projects/SuperBERT/mansisak/semantic_decay_models/bookcorpus_pretraining/'
             , help='Path to pretrained model weights (or valid name of hugging face pretrained model)')
 
--- train/trainer.py
+++ train/trainer.py
@@ -6,7 +6,13 @@ import sys
 sys.path.append('../')
 from data.load_data import get_data, process_abstracts, get_inputs, AbstractsDataset
 
-def train_model(epochs, model, loader):
+
+def train_model(epochs, model, batch_size, base_data_path, year, subject):
+    base_data_path = base_data_path
+    year = year
+    subject = subject
+    data_dir_path = base_data_path + year + "/" + subject + "/"
+
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     # and move our model over to the selected device
     model.to(device)
@@ -15,28 +21,43 @@ def train_model(epochs, model, loader):
     # initialize optimizer
     optim = torch.optim.AdamW(model.parameters(), lr=5e-6)
 
-    for epoch in range(epochs):
-        # setup loop with TQDM and dataloader
-        loop = tqdm(loader, leave=True)
-        for batch in loop:
-            # initialize calculated gradients (from prev step)
-            optim.zero_grad()
-            # pull all tensor batches required for training
-            input_ids = batch['input_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            labels = batch['labels'].to(device)
-            # process
-            outputs = model(input_ids, attention_mask=attention_mask,
-                            labels=labels)
-            # extract loss
-            loss = outputs.loss
-            # calculate loss for every parameter that needs grad update
-            loss.backward()
-            # update parameters
-            optim.step()
-            # print relevant info to progress bar
-            loop.set_description(f'Epoch {epoch}')
-            loop.set_postfix(loss=loss.item())
+    for filename in os.listdir(data_dir_path):
+        data_path = os.path.join(data_dir_path, filename)
+        # checking if it is a file
+        if os.path.isfile(data_path):
+            print(data_path)
+
+            bag, bag_size, abstracts = get_data(data_path)
+            sentence_a, sentence_b, label = process_abstracts(abstracts, bag, bag_size)
+            if(len(sentence_a) <= 0):
+                print("DATA LEN: ", len(sentence_a))
+                continue
+            inputs = get_inputs(sentence_a, sentence_b, label)
+            dataset = AbstractsDataset(inputs)
+            loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)  
+
+            for epoch in range(epochs):
+                # setup loop with TQDM and dataloader
+                loop = tqdm(loader, leave=True)
+                for batch in loop:
+                    # initialize calculated gradients (from prev step)
+                    optim.zero_grad()
+                    # pull all tensor batches required for training
+                    input_ids = batch['input_ids'].to(device)
+                    attention_mask = batch['attention_mask'].to(device)
+                    labels = batch['labels'].to(device)
+                    # process
+                    outputs = model(input_ids, attention_mask=attention_mask,
+                                    labels=labels)
+                    # extract loss
+                    loss = outputs.loss
+                    # calculate loss for every parameter that needs grad update
+                    loss.backward()
+                    # update parameters
+                    optim.step()
+                    # print relevant info to progress bar
+                    loop.set_description(f'Epoch {epoch}')
+                    loop.set_postfix(loss=loss.item())
     return model
 
 def eval_model(model, batch_size, base_data_path, year, subject):
